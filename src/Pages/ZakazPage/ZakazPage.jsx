ba2a9ab7609dc037ed95d392f6276a514ff6daa8
Rustambek Mamadaliyev <rustambek0301@gmail.com>
2022-04-14 10:58:53 +0500

    mahsulotlar page and responsive

--- src/Pages/ZakazPage/ZakazPage.jsx
+++ src/Pages/ZakazPage/ZakazPage.jsx
@@ -1,6 +1,6 @@
 import ZakazHeader from "../../Components/ZakazHeader/ZakazHeader";
 import ZakazTable from "../../Components/ZakazTable/ZakazTable";
-import "./ZakazPage.css";
+
 
 const ZakazPage = () =>{
 return (
