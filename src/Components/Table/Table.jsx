b5e5395cf69133ec86bdd58e30eec6119b53e459
Rustambek Mamadaliyev <rustambek0301@gmail.com>
2022-04-15 00:13:49 +0500

    add table.jsx completed and uncompleted

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -45,10 +45,9 @@ return(
           </button>
         </td>
         <td>
-          <label className="switch" data-id="1">
-            <input type="checkbox"     />
-            <span className="slider round"></span>
-          </label>
+       <span className="buyurtma-completed">
+         Completed
+       </span>
         </td>
       </tr>
       <tr>
@@ -70,108 +69,553 @@ return(
           </button>
         </td>
         <td>
-          <label className="switch" data-id="1">
-            <input type="checkbox"     />
-            <span className="slider round"></span>
-          </label>
-        </td>
-      </tr><tr>
-        <td data-title='ID'>
-          6
-        </td>
-        <td data-title='Sana'>
-          12:13-12.05.2021
-        </td>
-        <td data-title='Tovarlar Soni'>
-          10
-        </td>
-        <td data-title='Jami Summa'>
-          1 600 000 so’m
-        </td>
-        <td className='select'>
-          <button className='button'>
-            Buyurtma berish
-          </button>
-        </td>
-        <td>
-          <label className="switch" data-id="1">
-            <input type="checkbox"     />
-            <span className="slider round"></span>
-          </label>
-        </td>
-      </tr><tr>
-        <td data-title='ID'>
-          6
-        </td>
-        <td data-title='Sana'>
-          12:13-12.05.2021
-        </td>
-        <td data-title='Tovarlar Soni'>
-          10
-        </td>
-        <td data-title='Jami Summa'>
-          1 600 000 so’m
-        </td>
-        <td className='select'>
-          <button className='button'>
-            Buyurtma berish
-          </button>
-        </td>
-        <td>
-          <label className="switch" data-id="1">
-            <input type="checkbox"     />
-            <span className="slider round"></span>
-          </label>
-        </td>
-      </tr><tr>
-        <td data-title='ID'>
-          6
-        </td>
-        <td data-title='Sana'>
-          12:13-12.05.2021
-        </td>
-        <td data-title='Tovarlar Soni'>
-          10
-        </td>
-        <td data-title='Jami Summa'>
-          1 600 000 so’m
-        </td>
-        <td className='select'>
-          <button className='button'>
-            Buyurtma berish
-          </button>
-        </td>
-        <td>
-          <label className="switch" data-id="1">
-            <input type="checkbox"     />
-            <span className="slider round"></span>
-          </label>
-        </td>
-      </tr><tr>
-        <td data-title='ID'>
-          6
-        </td>
-        <td data-title='Sana'>
-          12:13-12.05.2021
-        </td>
-        <td data-title='Tovarlar Soni'>
-          10
-        </td>
-        <td data-title='Jami Summa'>
-          1 600 000 so’m
-        </td>
-        <td className='select'>
-          <button className='button'>
-            Buyurtma berish
-          </button>
-        </td>
-        <td>
-          <label className="switch">
-            <input type="checkbox" />
-            <span className="slider round"></span>
-          </label>
+       <span className=" buyurtma-uncompleted">
+         UnCompleted
+       </span>
         </td>
       </tr>
+      <tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className="buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr>
+      <tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className=" buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr>
+      <tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className="buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr><tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className=" buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr>
+      <tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className="buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr><tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className=" buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr>
+      <tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className="buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr><tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className=" buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr>
+      <tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className="buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr><tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className=" buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr>
+      <tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className="buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr><tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className=" buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr>
+      <tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className="buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr><tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className=" buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr>
+      <tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className="buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr><tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className=" buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr>
+      <tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className="buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr><tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className=" buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr>
+      <tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className="buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr><tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className=" buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr>
+      <tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className="buyurtma-uncompleted">
+         UnCompleted
+       </span>
+        </td>
+      </tr><tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className="buyurtma-completed">
+         Completed
+       </span>
+        </td>
+      </tr><tr>
+        <td data-title='ID'>
+          6
+        </td>
+        <td data-title='Sana'>
+          12:13-12.05.2021
+        </td>
+        <td data-title='Tovarlar Soni'>
+          10
+        </td>
+        <td data-title='Jami Summa'>
+          1 600 000 so’m
+        </td>
+        <td className='select'>
+          <button className='button'>
+            Buyurtma berish
+          </button>
+        </td>
+        <td>
+       <span className="buyurtma-completed">
+         Completed
+       </span>
+        </td>
+      </tr>
+       
 
     </tbody>
   </table>
