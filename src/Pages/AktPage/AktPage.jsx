20f58f084f6a16b09e203e7ce368aa11a12edc14
Rustambek Mamadaliyev <rustambek0301@gmail.com>
2022-04-19 22:27:53 +0500

    Add

--- src/Pages/AktPage/AktPage.jsx
+++ src/Pages/AktPage/AktPage.jsx
@@ -8,9 +8,7 @@ return(
 <Header />
     <div className="akt-top">
         <form className="akt-form">
-
-            {/* <input type="text" data-role="calendarpicker" placeholder="alooo" /> */}
-            <input className="akt-input" type="date" id="start" name="date-after" required placeholder="Date After" />
+    <input className="akt-input" type="date" id="start" name="date-after" required placeholder="Date After" />
      
             {/* <p className="akt-text">Date Before</p> */}
             <input className="akt-input" type="date" id="start" name="date-after" required placeholder="Date After" />
